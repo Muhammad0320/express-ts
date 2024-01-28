0db7e5d2ef37e8027313938a9392903607b6109d
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-28 08:57:24 +0100

    chore: update login Routes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from "express";
 import { router } from "./routes/loginRoutes";
+import bodyParser from "body-parser";
 
 const app = express();
 
 const port = 3000;
+
+bodyParser.urlencoded({ extended: true });
 
 app.use(router);
 
