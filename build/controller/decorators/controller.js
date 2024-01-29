d03269949e4f4c91ed0000ef8399d9938bea571f
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-29 12:22:41 +0100

    chore:  complete the controller decorator

--- build/controller/decorators/controller.js
+++ build/controller/decorators/controller.js
@@ -1,4 +1,13 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.controller = void 0;
+require("reflect-metadata");
 const controller = () => {
-    return function () { };
+    return function (target) {
+        Object.getOwnPropertyNames(target).forEach((key) => {
+            const routeHandler = target.prototype[key];
+            const path = Reflect.getMetadata("path", target.prototype, key);
+        });
+    };
 };
+exports.controller = controller;
--- src/controller/decorators/controller.ts
+++ src/controller/decorators/controller.ts
@@ -1,3 +1,11 @@
-const controller = () => {
-  return function () {};
+import "reflect-metadata";
+
+export const controller = () => {
+  return function (target: Function) {
+    Object.getOwnPropertyNames(target).forEach((key) => {
+      const routeHandler = target.prototype[key];
+
+      const path = Reflect.getMetadata("path", target.prototype, key);
+    });
+  };
 };
