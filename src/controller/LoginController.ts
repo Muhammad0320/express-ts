6a56bfe8ac15125ffd4cc39d91e2b498c11d59fb
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-29 11:43:11 +0100

    chore: update login controller

--- src/controller/LoginController.ts
+++ src/controller/LoginController.ts
@@ -1,1 +1,31 @@
-export class LoginController {}
+import { Request, Response } from "express";
+
+export class LoginController {
+  @get("/login")
+  get(req: Request, res: Response) {
+    res.send(`
+        
+              <form method='POST' >
+                  <div> 
+                      <label> Email </label>
+                      <input name='email' />
+                  </div>  
+                  <div>
+                      <label> Password </label> 
+                      <input name='password' type='password' />    
+               </div>
+      
+               <button> submit form </button>
+            </form>
+          
+        `);
+  }
+}
+
+const get = (path: string) => {
+  return function (target: LoginController, key: string) {};
+};
+
+const controller = (path: string) => {
+  return function (target: typeof LoginController, key: string) {};
+};
