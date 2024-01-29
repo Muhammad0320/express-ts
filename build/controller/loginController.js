52ebcd088dc96704a3fb11087a28ba88368086c0
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-29 11:31:50 +0100

    chore: update login controller

---
+++ build/controller/loginController.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LoginController = void 0;
+class LoginController {
+}
+exports.LoginController = LoginController;
