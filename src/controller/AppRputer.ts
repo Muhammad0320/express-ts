7d97846d69423f35bade2d5c28f0ae47c9e06e05
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-30 05:11:59 +0100

    chore: add a singleton for Express router

--- src/controller/AppRputer.ts
+++ src/controller/AppRputer.ts
@@ -1,1 +1,13 @@
-export class AppRouter {}
+import express from "express";
+
+export class AppRouter {
+  public static instance: express.Router;
+
+  static getInstance(): express.Router {
+    if (!this.instance) {
+      this.instance = express.Router();
+    }
+
+    return this.instance;
+  }
+}
