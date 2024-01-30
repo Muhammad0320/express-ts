b1844f5ce33b55f6f75182dae8b52683692e385c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-30 16:04:20 +0100

    chore: update controller ts file

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -29,43 +29,6 @@ router.get("/protected", AuthChecker, (req, res) => {
 
   `);
 });
-router.get("/login", (req, res) => {
-    res.send(`
-  
-        <form method='POST' >
-            <div> 
-                <label> Email </label>
-                <input name='email' />
-            </div>  
-            <div>
-                <label> Password </label> 
-                <input name='password' type='password' />    
-         </div>
-
-         <button> submit form </button>
-      </form>
-    
-  `);
-});
-router.post("/login", (req, res) => {
-    const { email, password } = req.body;
-    if (email &&
-        password &&
-        email === "muhammmad@gmail.com" &&
-        password === "password") {
-        req.session = { isLoggedIn: true };
-        res.redirect("/");
-    }
-    else {
-        res.send(`
-
-      <div>  
-          <div> Invalid login credentials try again </div>
-      </div>
-    
-    `);
-    }
-});
 router.get("/logout", (req, res) => {
     req.session = undefined;
     res.redirect("/login");
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -34,48 +34,6 @@ router.get("/protected", AuthChecker, (req: Request, res: Response) => {
   `);
 });
 
-router.get("/login", (req: Request, res: Response) => {
-  res.send(`
-  
-        <form method='POST' >
-            <div> 
-                <label> Email </label>
-                <input name='email' />
-            </div>  
-            <div>
-                <label> Password </label> 
-                <input name='password' type='password' />    
-         </div>
-
-         <button> submit form </button>
-      </form>
-    
-  `);
-});
-
-router.post("/login", (req: RequestWithBody, res: Response) => {
-  const { email, password } = req.body;
-
-  if (
-    email &&
-    password &&
-    email === "muhammmad@gmail.com" &&
-    password === "password"
-  ) {
-    req.session = { isLoggedIn: true };
-
-    res.redirect("/");
-  } else {
-    res.send(`
-
-      <div>  
-          <div> Invalid login credentials try again </div>
-      </div>
-    
-    `);
-  }
-});
-
 router.get("/logout", (req: Request, res: Response) => {
   req.session = undefined;
 
