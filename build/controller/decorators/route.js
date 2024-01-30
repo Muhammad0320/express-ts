f157bc215318a4d7b38563aacd4c978948726030
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-30 06:40:18 +0100

    chore: update get name to route

---
+++ build/controller/decorators/route.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.patch = exports.del = exports.post = exports.put = exports.get = void 0;
+require("reflect-metadata");
+const Methods_1 = require("./Methods");
+const buildMethod = function (method) {
+    return function (path) {
+        return function (target, key) {
+            Reflect.defineMetadata("path", path, target, key);
+            Reflect.defineMetadata("method", method, target, key);
+        };
+    };
+};
+exports.get = buildMethod(Methods_1.Methods.get);
+exports.put = buildMethod(Methods_1.Methods.put);
+exports.post = buildMethod(Methods_1.Methods.post);
+exports.del = buildMethod(Methods_1.Methods.delete);
+exports.patch = buildMethod(Methods_1.Methods.patch);
