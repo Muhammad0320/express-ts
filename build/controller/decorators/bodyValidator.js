def660438617d85be99678ff3ff9286c2b45836f
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-30 15:03:39 +0100

    chore: add body validator file

---
+++ build/controller/decorators/bodyValidator.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.bodyValidator = void 0;
+const bodyValidator = (...keys) => { };
+exports.bodyValidator = bodyValidator;
---
+++ src/controller/decorators/bodyValidator.ts
@@ -0,0 +1,1 @@
+export const bodyValidator = (...keys: string[]) => {};
