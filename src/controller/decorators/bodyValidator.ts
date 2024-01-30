c5574019e666294484684f8890075dccc5f81ed2
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-30 15:06:38 +0100

    chore: update body Validator

--- src/controller/decorators/bodyValidator.ts
+++ src/controller/decorators/bodyValidator.ts
@@ -1,1 +1,8 @@
-export const bodyValidator = (...keys: string[]) => {};
+import "reflect-metadata";
+import { MetadataKeys } from "./MetadataKeys";
+
+export const bodyValidator = (...keys: string[]) => {
+  return function (target: any, key: string, desc: PropertyDescriptor) {
+    Reflect.defineMetadata(MetadataKeys.validator, keys, target, key);
+  };
+};
