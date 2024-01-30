86aca9e88cdfb2d7fd4a785265bac02e28d889fb
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-30 06:53:46 +0100

    chore: update use ts file

--- build/controller/decorators/use.js
+++ build/controller/decorators/use.js
@@ -1,7 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.use = void 0;
-const use = (middle) => {
-    return function () { };
+require("reflect-metadata");
+const MetadataKeys_1 = require("./MetadataKeys");
+const use = (middleware) => {
+    return function (target, key) {
+        const middlewares = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.middleware, target.prototype, key) || [];
+        Reflect.defineMetadata(MetadataKeys_1.MetadataKeys.middleware, [...middlewares, middleware], target, key);
+    };
 };
 exports.use = use;
--- src/controller/decorators/use.ts
+++ src/controller/decorators/use.ts
@@ -1,5 +1,18 @@
 import { RequestHandler } from "express";
 
-export const use = (middle: RequestHandler) => {
-  return function () {};
+import "reflect-metadata";
+import { MetadataKeys } from "./MetadataKeys";
+
+export const use = (middleware: RequestHandler) => {
+  return function (target: any, key: string) {
+    const middlewares =
+      Reflect.getMetadata(MetadataKeys.middleware, target.prototype, key) || [];
+
+    Reflect.defineMetadata(
+      MetadataKeys.middleware,
+      [...middlewares, middleware],
+      target,
+      key
+    );
+  };
 };
