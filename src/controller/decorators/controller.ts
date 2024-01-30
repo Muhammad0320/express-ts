844591fd546431d60db9733cf3edc22cdaeeb779
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-30 04:23:48 +0100

    chore: update controller ts file

--- src/controller/decorators/controller.ts
+++ src/controller/decorators/controller.ts
@@ -1,11 +1,17 @@
 import "reflect-metadata";
 
+import express from "express";
+
 export const controller = (RouterPrefix: string) => {
   return function (target: Function) {
     Object.getOwnPropertyNames(target).forEach((key) => {
       const routeHandler = target.prototype[key];
 
       const path = Reflect.getMetadata("path", target.prototype, key);
+
+      const router = express.Router();
+
+      router.get(`${RouterPrefix}${path}`, routeHandler);
     });
   };
 };
