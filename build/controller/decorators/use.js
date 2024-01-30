5b8b4f19685fa4f726ac0e2b07dc1fe8cebd92ac
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-30 07:18:25 +0100

    chore: bug found but not fixed AllihamdulliLah

--- build/controller/decorators/use.js
+++ build/controller/decorators/use.js
@@ -5,7 +5,10 @@ require("reflect-metadata");
 const MetadataKeys_1 = require("./MetadataKeys");
 const use = (middleware) => {
     return function (target, key) {
+        console.log("show your self sucker 2");
         const middlewares = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.middleware, target.prototype, key) || [];
+        console.log(middlewares);
+        console.log("show your self sucker 1");
         Reflect.defineMetadata(MetadataKeys_1.MetadataKeys.middleware, [...middlewares, middleware], target, key);
     };
 };
--- src/controller/decorators/use.ts
+++ src/controller/decorators/use.ts
@@ -5,8 +5,14 @@ import { MetadataKeys } from "./MetadataKeys";
 
 export const use = (middleware: RequestHandler) => {
   return function (target: any, key: string) {
+    console.log("show your self sucker 2");
+
     const middlewares =
       Reflect.getMetadata(MetadataKeys.middleware, target.prototype, key) || [];
+
+    console.log(middlewares);
+
+    console.log("show your self sucker 1");
 
     Reflect.defineMetadata(
       MetadataKeys.middleware,
